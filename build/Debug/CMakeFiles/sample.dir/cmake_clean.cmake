b8e82708d5f5f23e204e6ede9f05b94c0bd1c0ad
Abel Romero Galvez <mac@Mac-mini-de-Abel.local>
2026-09-17 08:39:41 -0500

    comments added..

---
+++ build/Debug/CMakeFiles/sample.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sample.dir/src/platform/macos/transport_macos.cpp.o"
+  "CMakeFiles/sample.dir/src/platform/macos/transport_macos.cpp.o.d"
+  "CMakeFiles/sample.dir/src/sample.cpp.o"
+  "CMakeFiles/sample.dir/src/sample.cpp.o.d"
+  "CMakeFiles/sample.dir/src/websocket/lws_websocket_client.cpp.o"
+  "CMakeFiles/sample.dir/src/websocket/lws_websocket_client.cpp.o.d"
+  "libsample.dylib"
+  "libsample.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sample.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
